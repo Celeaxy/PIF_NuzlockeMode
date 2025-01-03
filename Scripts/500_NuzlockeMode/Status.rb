91f12156d7ab413836d61532acd377b548497943
Celeaxy <byt.es@mailbox.org>
2025-01-03 17:26:12 +0100

    Display dead status in pokemon storage scene & remove unused code

--- Scripts/500_NuzlockeMode/Status.rb
+++ Scripts/500_NuzlockeMode/Status.rb
@@ -1,3 +1,13 @@
+# ----------------------------------------------------------------------------------------------------
+# TODO:
+#   - Prohibit fusing dead pokemon while accessing PC
+#   - Prohibit taking dead pokemon into party
+# ----------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------
+# NuzlockeMode status module
+# ----------------------------------------------------------------------------------------------------
+
 GameData::Status.register({
   :id        => :POKERUS,
   :id_number => 6,
@@ -200,94 +210,51 @@ class PokemonPartyPanel
   end
 end
 
-# class PokemonSummary_Scene
-#   def drawPage(page)
-#     if @pokemon.egg?
-#       drawPageOneEgg
-#       return
-#     end
-#     @sprites["itemicon"].item = @pokemon.item_id
-#     overlay = @sprites["overlay"].bitmap
-#     overlay.clear
-#     base = Color.new(248, 248, 248)
-#     shadow = Color.new(104, 104, 104)
-#     # Set background image
-#     @sprites["background"].setBitmap("Graphics/Pictures/Summary/bg_#{page}") if page < NB_PAGES
-#     imagepos = []
-#     # Show the Poké Ball containing the Pokémon
-#     ballimage = sprintf("Graphics/Pictures/Summary/icon_ball_%s", @pokemon.poke_ball)
-#     if !pbResolveBitmap(ballimage)
-#       ballimage = sprintf("Graphics/Pictures/Summary/icon_ball_%02d", pbGetBallType(@pokemon.poke_ball))
-#     end
-#     imagepos.push([ballimage, 14, 60])
-#     # Show status/fainted/Pokérus infected icon
-#     status = 0
-#     if @pokemon.fainted?
-#       status = GameData::Status.get(:BURN).id_number
-#     elsif @pokemon.status != :NONE
-#       status = GameData::Status.get(@pokemon.status).id_number
-#     elsif @pokemon.pokerusStage == 1
-#       status = GameData::Status::DATA.keys.length / 2 + 1
-#     end
-#     status -= 1
-#     if status >= 0
-#       imagepos.push(["Data/Scripts/NuzlockeMod/statuses", 124, 100, 0, 16 * status, 44, 16])
-#     end
-#     # Show Pokérus cured icon
-#     if @pokemon.pokerusStage == 2
-#       imagepos.push([sprintf("Graphics/Pictures/Summary/icon_pokerus"), 176, 100])
-#     end
-#     # Show shininess star
-#     if @pokemon.shiny?
-#       addShinyStarsToGraphicsArray(imagepos, 2, 134, @pokemon.bodyShiny?, @pokemon.headShiny?, @pokemon.debugShiny?, nil, nil, nil, nil, true)
-#       #imagepos.push([sprintf("Graphics/Pictures/shiny"), 2, 134])
-#     end
-#     # Draw all images
-#     pbDrawImagePositions(overlay, imagepos)
-#     # Write various bits of text
-#     pagename = [_INTL("INFO"),
-#                 _INTL("TRAINER MEMO"),
-#                 _INTL("SKILLS"),
-#                 _INTL("MOVES"),
-#                 _INTL("MOVES")][page - 1]
-#     textpos = [
-#       [pagename, 26, 10, 0, base, shadow],
-#       [@pokemon.name, 46, 56, 0, base, shadow],
-#       [@pokemon.level.to_s, 46, 86, 0, Color.new(64, 64, 64), Color.new(176, 176, 176)],
-#       [_INTL("Item"), 66, 312, 0, base, shadow]
-#     ]
-#     # Write the held item's name
-#     if @pokemon.hasItem?
-#       textpos.push([@pokemon.item.name, 16, 346, 0, Color.new(64, 64, 64), Color.new(176, 176, 176)])
-#     else
-#       textpos.push([_INTL("None"), 16, 346, 0, Color.new(192, 200, 208), Color.new(208, 216, 224)])
-#     end
-#     # Write the gender symbol
-#     if @pokemon.male?
-#       textpos.push([_INTL("♂"), 178, 56, 0, Color.new(24, 112, 216), Color.new(136, 168, 208)])
-#     elsif @pokemon.female?
-#       textpos.push([_INTL("♀"), 178, 56, 0, Color.new(248, 56, 32), Color.new(224, 152, 144)])
-#     end
-#     # Draw all text
-#     pbDrawTextPositions(overlay, textpos)
-#     # Draw the Pokémon's markings
-#     drawMarkings(overlay, 84, 292)
-#     # Draw page-specific information
-#     case page
-#     when 1 then
-#       drawPageOne
-#     when 2 then
-#       drawPageTwo
-#     when 3 then
-#       drawPageThree
-#     when 4 then
-#       drawPageFour
-#     when 5 then
-#       drawPageFive
-#     end
-#   end
-# end
 
+# ----------------------------------------------------------------------------------------------------
+# Pokémon storage
+# ----------------------------------------------------------------------------------------------------
+
+class SpriteWrapper
+    # needed to access the dimensions of the scaled pokemon sprite
+    def sprite
+      @sprite 
+    end
+end
+
+
+class PokemonStorageScene
+  alias_method :original_pbUpdateOverlay, :pbUpdateOverlay
+  def pbUpdateOverlay(selection, party = nil)
+    original_pbUpdateOverlay(selection, party)
+    return if !NuzlockeMode.active?
+    pokemon = nil
+    if @screen.pbHeldPokemon && !@screen.fusionMode
+      pokemon = @screen.pbHeldPokemon
+    elsif selection >= 0
+      pokemon = (party) ? party[selection] : @storage[@storage.currentBox, selection]
+    end
+    
+    if !@sprites["dead_status"] # create only once
+      return if !pokemon
+      status = GameData::Status.get(:DEAD).id_number
+      x = 90 - 22
+      y = 134 + @sprites["pokemon"].sprite.fullheight / 2 - 30
+      @sprites["dead_status"] = IconSprite.new(x, y, @boxsidesviewport)
+      @sprites["dead_status"].setBitmap("Data/NuzlockeMode_Data/statuses")
+      @sprites["dead_status"].src_rect = Rect.new(0, 16 * status, 44, 16)
+    end
+      
+    @sprites["dead_status"].visible = pokemon && pokemon.fainted?
+  end
+end
+
+# ----------------------------------------------------------------------------------------------------
+# Battle
+# ----------------------------------------------------------------------------------------------------
+
+# Pretty intrusive to change only a message, but w/e
+# 
 class PokeBattle_Battler
   def pbFaint(showMessage=true)
     if !fainted?
